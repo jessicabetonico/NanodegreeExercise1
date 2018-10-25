1cc50e311a08151b470f06143771ff991030a473
jessicabetonico <jessicabetonico@gmail.com>
2018-10-25 03:49:06 +0000

    Algumas melhorias

--- src/UsersMovie.js
+++ src/UsersMovie.js
@@ -2,10 +2,11 @@ import React, {Component} from 'react'
 
 class UsersMovie extends Component {
   render(){
+    const {profiles, users, movies} = this.props
     return(
       <ul>
-      	{this.props.profiles.map((profile) => (      		
-      		<li>{ `${this.props.users[profile.userID].name} favorite movie is ${this.props.movies[profile.favoriteMovieID].name} `}</li>      
+      	{profiles.map((profile) => (      		
+      		<li key={profile.userID}> <p>{ `${users[profile.userID].name} favorite movie is ${movies[profile.favoriteMovieID].name} `}</p></li>      
       	))}
       </ul>     
     )
